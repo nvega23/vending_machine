eb926114ad7da3116f55b302b7e667ecc7e66a0a
Nestor Vega <vega.nestor1@gmail.com>
2023-10-17 18:48:55 -0700

    changing background color

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls } from '@react-three/drei';
 import { TextureLoader } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export const Experience = () => {
     return (
